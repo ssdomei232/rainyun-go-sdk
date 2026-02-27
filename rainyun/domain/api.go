f11296823f2fec88da5cd815167922a11cf7390c
mei <i@mmeiblog.cn>
2026-02-27 21:34:00 +0800

    feat(domain): 新增部分域名接口

--- rainyun/domain/api.go
+++ rainyun/domain/api.go
@@ -70,3 +70,199 @@ func (c *Client) VerifyDomainCertify(domain string) (*common.BasicOperationRespo
 
 	return &resp, err
 }
+
+// 添加域名DNS解析
+//
+// id: 域名ID
+func (c *Client) AddDomainDNSRecord(id int, req *AddDomainDNSRecordRequest) (*common.BasicOperationResponse, error) {
+	path := fmt.Sprintf("/product/domain/%d/dns", id)
+
+	var resp common.BasicOperationResponse
+	err := c.DoRequest("POST", path, req, &resp)
+
+	return &resp, err
+}
+
+// 修改域名DNS解析
+func (c *Client) UpdateDomainDNSRecord(id int, req *AddDomainDNSRecordRequest) (*common.BasicOperationResponse, error) {
+	path := fmt.Sprintf("/product/domain/%d/dns", id)
+
+	var resp common.BasicOperationResponse
+	err := c.DoRequest("PATCH", path, req, &resp)
+
+	return &resp, err
+}
+
+// 删除域名DNS解析
+func (c *Client) DeleteDomainDNSRecord(id int, recordID int) (*common.BasicOperationResponse, error) {
+	path := fmt.Sprintf("/product/domain/%d/dns", id)
+
+	var resp common.BasicOperationResponse
+	err := c.DoRequest("DELETE", path, DeleteDomainDNSRecordRequest{RecordID: recordID}, &resp)
+
+	return &resp, err
+}
+
+// 添加域名DNSSEC
+func (c *Client) AddDomainDNSSEC(id int, req *AddDomainDNSSECRequest) (*common.BasicOperationResponse, error) {
+	path := fmt.Sprintf("/product/domain/%d/dnssec", id)
+
+	var resp common.BasicOperationResponse
+	err := c.DoRequest("POST", path, req, &resp)
+
+	return &resp, err
+}
+
+// 删除域名DNSSEC
+func (c *Client) DeleteDomainDNSSEC(id int, req *DeleteDomainDNSSECRequest) (*common.BasicOperationResponse, error) {
+	path := fmt.Sprintf("/product/domain/%d/dnssec/delete", id)
+
+	var resp common.BasicOperationResponse
+	err := c.DoRequest("POST", path, req, &resp)
+
+	return &resp, err
+}
+
+// 同步域名DNSSEC
+func (c *Client) SyncDomainDNSSEC(id int) (*common.BasicOperationResponse, error) {
+	path := fmt.Sprintf("/product/domain/%d/dnssec/sync", id)
+
+	var resp common.BasicOperationResponse
+	err := c.DoRequest("POST", path, nil, &resp)
+
+	return &resp, err
+}
+
+// 关闭域名锁定
+func (c *Client) UnlockDomain(id int) (*common.BasicOperationResponse, error) {
+	path := fmt.Sprintf("/product/domain/%d/lock/disable", id)
+
+	var resp common.BasicOperationResponse
+	err := c.DoRequest("PUT", path, nil, &resp)
+
+	return &resp, err
+}
+
+// 开启域名锁定
+func (c *Client) LockDomain(id int) (*common.BasicOperationResponse, error) {
+	path := fmt.Sprintf("/product/domain/%d/lock/enable", id)
+
+	var resp common.BasicOperationResponse
+	err := c.DoRequest("PUT", path, nil, &resp)
+
+	return &resp, err
+}
+
+// 修改域名NS服务器
+func (c *Client) UpdateDomainNS(id int, nss []string) (*common.BasicOperationResponse, error) {
+	path := fmt.Sprintf("/product/domain/%d/nameservers", id)
+
+	var resp common.BasicOperationResponse
+	err := c.DoRequest("PUT", path, UpdateDomainNSRequest{NameServers: nss}, &resp)
+
+	return &resp, err
+}
+
+// 重置域名NS服务器
+func (c *Client) ResetDomainNS(id int) (*common.BasicOperationResponse, error) {
+	path := fmt.Sprintf("/product/domain/%d/nameservers/reset", id)
+
+	var resp common.BasicOperationResponse
+	err := c.DoRequest("POST", path, nil, &resp)
+
+	return &resp, err
+}
+
+// 续费域名
+func (c *Client) RenewDomain(id int, years int) (*common.BasicOperationResponse, error) {
+	path := fmt.Sprintf("/product/domain/%d/renew", id)
+
+	var resp common.BasicOperationResponse
+	err := c.DoRequest("POST", path, map[string]int{"years": years}, &resp)
+
+	return &resp, err
+}
+
+// 域名过户
+func (c *Client) TransferDomain(id int, req *DomainTransferRequest) (*common.BasicOperationResponse, error) {
+	path := fmt.Sprintf("/product/domain/%d/transfer", id)
+
+	var resp common.BasicOperationResponse
+	err := c.DoRequest("POST", path, req, &resp)
+
+	return &resp, err
+}
+
+// 查询域名模板列表
+//
+// options: 查询参数 可以用 EncodingStandardQueryParameters 获取.
+func (c *Client) GetDomainTemplateList(options string) (*DomainTemplateList, error) {
+	path := fmt.Sprintf("/product/domain/template/?options=%s", options)
+
+	var resp DomainTemplateList
+	err := c.DoRequest("GET", path, nil, &resp)
+
+	return &resp, err
+}
+
+// 删除域名信息模板
+func (c *Client) DeleteDomainTemplate(sysID string) (*common.BasicOperationResponse, error) {
+	path := "/product/domain/template/"
+
+	var resp common.BasicOperationResponse
+	err := c.DoRequest("DELETE", path, DeleteDomainTemplateRequest{SysID: sysID}, &resp)
+
+	return &resp, err
+}
+
+// 更新域名管理密码
+func (c *Client) UpdateDomainPassword(id int, password string) (*common.BasicOperationResponse, error) {
+	path := fmt.Sprintf("/product/domain/%d/password", id)
+
+	var resp common.BasicOperationResponse
+	err := c.DoRequest("POST", path, UpdateDomainPasswordRequest{Password: password}, &resp)
+
+	return &resp, err
+}
+
+// 编辑域名模板
+func (c *Client) EditDomainTemplate(sysID string, req *EditDomainTemplateRequest) (*common.BasicOperationResponse, error) {
+	path := "/product/domain/template"
+
+	var resp common.BasicOperationResponse
+	err := c.DoRequest("PUT", path, req, &resp)
+
+	return &resp, err
+}
+
+// 获取域名模板详情
+func (c *Client) GetDomainTemplateDetail(sysID string) (*GetDomainTemplateDetailResponse, error) {
+	path := fmt.Sprintf("/product/domain/template/detail/?sys_id=%s", sysID)
+
+	var resp GetDomainTemplateDetailResponse
+	err := c.DoRequest("GET", path, nil, &resp)
+
+	return &resp, err
+}
+
+// 获取域名whois信息
+func (c *Client) GetDomainWhoisInfo(domain string) (*DomainWhoisInfo, error) {
+	path := fmt.Sprintf("/product/domain/whois?domain=%s", domain)
+
+	var resp DomainWhoisInfo
+	err := c.DoRequest("GET", path, nil, &resp)
+
+	return &resp, err
+}
+
+// 检查域名能否注册
+func (c *Client) CheckDomainAvailability(req *CheckDomainAvailableRequest) (*CheckDomainAvailableResponse, error) {
+	path := "/product/domain/check"
+
+	var resp CheckDomainAvailableResponse
+	err := c.DoRequest("GET", path, req, &resp)
+
+	return &resp, err
+}
+
+// TODO: 列出域名列表,获取域名详情,下载域名证书，获取域名DNS解析记录列表，获取域名DNSSEC详情,获取域名续费价格,域名注册,获取域名管理密码
--- rainyun/domain/model.go
+++ rainyun/domain/model.go
@@ -51,3 +51,171 @@ type DomainVerificationInfo struct {
 type AddDomainVerificationRequest struct {
 	Domain string `json:"domain"`
 }
+
+// 添加域名DNS解析请求
+type AddDomainDNSRecordRequest struct {
+	Host            string `json:"host"`              // 主机名
+	Level           int    `json:"level"`             // 优先等级
+	Line            string `json:"line"`              // 线路，必须有一个默认的才可以 DEFAULT/LTEL/LCNC/LMOB/LEDU/LSEO/LFOR
+	RainProductID   int    `json:"rain_product_id"`   // 雨云产品id(搭配其它产品时使用)
+	RainProductType string `json:"rain_product_type"` // 雨云产品类型(搭配其它产品时使用) rcs/rvh/rgs/rbm
+	RecordID        int    `json:"record_id"`         // 记录id(修改时使用)
+	TTL             int    `json:"ttl"`               // 解析生效时间
+	Type            string `json:"type"`              // 解析类型 A/AAAA/CNAME/TXT/MX/SRV
+	Value           string `json:"value"`             // 解析值
+}
+
+// 删除域名DNS解析请求
+type DeleteDomainDNSRecordRequest struct {
+	RecordID int `json:"record_id"` // 记录id
+}
+
+// 添加域名DNSSEC请求
+type AddDomainDNSSECRequest struct {
+	Domain    string `json:"domain"`
+	Keyalg    string `json:"keyalg"`
+	Keydigest string `json:"keydigest"`
+	Keytag    string `json:"keytag"`
+	Keytype   string `json:"keytype"`
+}
+
+// 删除域名DNSSEC请求
+type DeleteDomainDNSSECRequest struct {
+	Domain    string `json:"domain"`
+	Keydigest string `json:"keydigest"`
+	Keytag    int    `json:"keytag"`
+}
+
+// 修改域名NS服务器
+type UpdateDomainNSRequest struct {
+	NameServers []string `json:"name_servers"`
+}
+
+// 域名过户
+type DomainTransferRequest struct {
+	NewTemplateInfo struct {
+		Address       string `json:"address"`         // 中文，通讯地址
+		City          string `json:"city"`            // 中文，市
+		CompanyEnName string `json:"company_en_name"` // 企业英文名称，如果类型是企业就要传入
+		CompanyName   string `json:"company_name"`    // 企业名称，如果类型是企业就要传入
+		Country       string `json:"country"`         // 所在国家
+		Email         string `json:"email"`           // 邮箱
+		IDImg         string `json:"id_img"`          // base64编码的证件照片
+		IDNum         string `json:"id_num"`          // 证件值
+		IDType        string `json:"id_type"`         // 证件类型，如SFZ
+		Name          string `json:"name"`            // 联系人名
+		Phone         string `json:"phone"`           // 联系电话
+		Province      string `json:"province"`        // 中文，省
+		SysID         string `json:"sys_id"`          // 模板标识(修改时使用)
+		Type          string `json:"type"`            // 所有者类型，I是个人，E是企业
+		ZipCode       string `json:"zip_code"`        // 中文，邮编
+	} `json:"new_template_info"` // 和SysID二选一，如果没模板就用这个新建模板
+	SysID string `json:"sys_id"` // 新域名模板ID
+}
+
+// 域名模板列表
+type DomainTemplateList struct {
+	Code int `json:"code"`
+	Data struct {
+		TotalRecords int                    `json:"TotalRecords"`
+		Records      []DomainTemplateDetail `json:"Records"` // 域名模板详情
+	} `json:"data"`
+}
+
+// 删除域名信息模板
+type DeleteDomainTemplateRequest struct {
+	SysID string `json:"sys_id"` // 模板标识
+}
+
+// 获取域名模板详情响应
+type GetDomainTemplateDetailResponse struct {
+	Code int                  `json:"code"`
+	Data DomainTemplateDetail `json:"data"`
+}
+
+// 域名模板详情
+type DomainTemplateDetail struct {
+	ID          int    `json:"id"`
+	UID         int    `json:"uid"`
+	AddTime     int    `json:"add_time"`     // 添加时间
+	SysID       string `json:"sys_id"`       // 模板标识
+	Type        string `json:"type"`         // 所有者类型，I是个人，E是企业
+	Owner       string `json:"owner"`        // 模板所有者
+	ContactName string `json:"contact_name"` //	 联系人姓名
+	CompanyName string `json:"company_name"` // 企业名称，如果是企业模板就有这个
+	Email       string `json:"email"`        // 联系人邮箱
+	Country     string `json:"country"`      // 所在国家
+	Province    string `json:"province"`     // 中文，省
+	City        string `json:"city"`         // 中文，市
+	ZipCode     string `json:"zip_code"`     // 中文，邮编
+	Phone       string `json:"phone"`        // 联系电话
+	Address     string `json:"address"`      // 中文，通讯地址
+	IDType      string `json:"id_type"`      // 证件类型，如SFZ
+	IDNum       string `json:"id_num"`       // 证件值
+	CStatus     string `json:"c_status"`     // 不明， 1
+	CFailInfo   string `json:"c_fail_info"`  // 模板失败信息
+	RStatus     string `json:"r_status"`     // 不明, 1
+	RFailInfo   string `json:"r_fail_info"`  // 不明
+}
+
+// 更新域名管理密码
+type UpdateDomainPasswordRequest struct {
+	Password string `json:"password"`
+}
+
+// 编辑域名模板
+type EditDomainTemplateRequest struct {
+	Address       string `json:"address"`         // 中文，通讯地址
+	City          string `json:"city"`            // 中文，市
+	CompanyEnName string `json:"company_en_name"` // 企业英文名称，如果类型是企业就要传入
+	CompanyName   string `json:"company_name"`    // 企业名称，如果类型是企业就要传入
+	Country       string `json:"country"`         // 所在国家
+	Email         string `json:"email"`           // 联系人邮箱
+	IDImg         string `json:"id_img"`          // base64编码的证件照片
+	IDNum         string `json:"id_num"`          // 证件值
+	IDType        string `json:"id_type"`         // 证件类型，如SFZ
+	Name          string `json:"name"`            // 联系人名
+	Phone         string `json:"phone"`           // 联系电话
+	Province      string `json:"province"`        // 中文，省
+	SysID         string `json:"sys_id"`          // 模板标识(修改时使用)
+	Type          string `json:"type"`            // 所有者类型，I是个人，E是企业
+	ZipCode       string `json:"zip_code"`        // 中文，邮编
+}
+
+// 域名WHOIS信息
+type DomainWhoisInfo struct {
+	Code int `json:"code"`
+	Data struct {
+		RegDate    string `json:"reg_date"`   // 注册时间
+		ExpDate    string `json:"exp_date"`   // 到期时间
+		Status     string `json:"status"`     // 域名状态
+		Owner      string `json:"owner"`      // 域名所有者
+		Registrar  string `json:"registrar"`  // 域名注册商
+		Email      string `json:"email"`      // 域名所有者邮箱
+		Nameserver string `json:"nameserver"` // 域名服务器
+		BizServer  string `json:"biz_server"`
+		Body       string `json:"body"`    // 域名WHOIS信息原文
+		Updated    string `json:"updated"` // WHOIS信息更新时间
+	} `json:"data"`
+}
+
+// 检查域名能否注册请求
+type CheckDomainAvailableRequest struct {
+	Domain string `json:"domain"` // 要检查的域名（中文域名同时支持汉字及punycode转码）
+	Suffix string `json:"suffix"` // 要检查的后缀，多个的话，用英文逗号分割
+}
+
+// 检查域名能否注册响应
+type CheckDomainAvailableResponse struct {
+	Code int `json:"code"`
+	Data []struct {
+		IsAvail     bool   `json:"is_avail"` // 是否可注册
+		SpecialType string `json:"special_type"`
+		Name        string `json:"name"` // 域名
+		Price       struct {
+			FirstYearPrice int `json:"first_year_price"` // 首年价格
+			BuyPrice       int `json:"buy_price"`        // 购买价格
+			RenewPrice     int `json:"renew_price"`      // 续费价格
+		} `json:"price"`
+	} `json:"data"`
+}
